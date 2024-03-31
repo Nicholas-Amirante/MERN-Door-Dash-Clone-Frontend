f4ec5bc2a20656535b8afbaf3126ced0597d2aaa
Nick-learns-code <nicholasamirante@gmail.com>
2024-03-31 15:27:38 -0600

    adding user profile form feature

---
+++ src/pages/ManageRestaurantPage.tsx
@@ -0,0 +1,20 @@
+import { useCreateMyRestaurant, useGetMyRestaurant, useUpdateMyRestaurant } from "@/api/MyRestaurantApi";
+import ManageRestaurantForm from "@/forms/manage-restaurant-form/ManageRestaurantForm";
+
+const ManageRestaurantPage = () => {
+  const { createRestaurant, isLoading: isCreateLoading } = useCreateMyRestaurant();
+  const { restaurant } = useGetMyRestaurant();
+  const { updateRestaurant, isLoading: IsUpdateLoading} = useUpdateMyRestaurant();
+
+  const isEditing = !!restaurant;
+
+  return (
+  <ManageRestaurantForm 
+  restaurant={restaurant} 
+  onSave={isEditing ? updateRestaurant : createRestaurant} 
+  isLoading={isCreateLoading || IsUpdateLoading}/>
+  );
+  
+}
+
+export default ManageRestaurantPage;
