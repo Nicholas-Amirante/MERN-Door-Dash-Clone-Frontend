a2a0337cc517137c5a6d4b72431c439e12f8fa71
Nick-learns-code <nicholasamirante@gmail.com>
2024-04-07 06:08:13 -0600

    updated order page

--- src/pages/ManageRestaurantPage.tsx
+++ src/pages/ManageRestaurantPage.tsx
@@ -1,4 +1,6 @@
-import { useCreateMyRestaurant, useGetMyRestaurant, useUpdateMyRestaurant } from "@/api/MyRestaurantApi";
+import { useCreateMyRestaurant, useGetMyRestaurant, useGetMyRestaurantORders, useUpdateMyRestaurant } from "@/api/MyRestaurantApi";
+import OrderItemCard from "@/components/OrderItemCard";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import ManageRestaurantForm from "@/forms/manage-restaurant-form/ManageRestaurantForm";
 
 const ManageRestaurantPage = () => {
@@ -6,13 +8,29 @@ const ManageRestaurantPage = () => {
   const { restaurant } = useGetMyRestaurant();
   const { updateRestaurant, isLoading: IsUpdateLoading} = useUpdateMyRestaurant();
 
+  const { orders } = useGetMyRestaurantORders();
+
   const isEditing = !!restaurant;
 
   return (
-  <ManageRestaurantForm 
-  restaurant={restaurant} 
-  onSave={isEditing ? updateRestaurant : createRestaurant} 
-  isLoading={isCreateLoading || IsUpdateLoading}/>
+    <Tabs defaultValue="orders">
+      <TabsList>
+        <TabsTrigger value="orders">Orders</TabsTrigger>
+        <TabsTrigger value="manage-restaurant">Manage Restaurant</TabsTrigger>
+      </TabsList>
+      <TabsContent value="orders" className="space-y-5 bg-gray-50 pg-10 rounded-lg">
+        <h2 className="text-2xl font-bold">{orders?.length} active orders</h2>
+        {orders?.map((order)=> <OrderItemCard order={order}/>)}
+      </TabsContent>
+      <TabsContent value="manage-restaurant">
+      <ManageRestaurantForm 
+    restaurant={restaurant} 
+    onSave={isEditing ? updateRestaurant : createRestaurant} 
+    isLoading={isCreateLoading || IsUpdateLoading}/>
+      </TabsContent>
+    
+    </Tabs>
+ 
   );
   
 }
